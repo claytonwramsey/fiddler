d8335f3eff5f620564b1332fc237d474081d9adf
claytonwramsey <claytonwramsey@gmail.com>
2021-08-02 22:31:16 -0500

    masking on rook moves

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,10 +1,11 @@
-use std::ops::BitAnd;
+use std::ops::{BitAnd, BitOr};
+use std::fmt::{Display, Formatter, Result};
 use crate::square::Square;
 
 /* a bitboard to express positions
  * uses standard form, so H8G8F8 (...) C1B1A1.
  */
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct Bitboard(pub u64);
 
 impl BitAnd for Bitboard {
@@ -15,9 +16,29 @@ impl BitAnd for Bitboard {
     }
 }
 
+impl BitOr for Bitboard {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        return Self(self.0 | rhs.0);
+    }
+}
+
+impl PartialEq for Bitboard {
+    fn eq(&self, rhs: &Bitboard) -> bool {
+        return self.0 == rhs.0;
+    }
+}
+impl Eq for Bitboard {}
 
 impl From<Square> for Bitboard {
     fn from(sq: Square) -> Bitboard {
         Bitboard(1 << sq.0)
+    }
+}
+
+impl Display for Bitboard {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "0x{:16x}", self.0)
     }
 }
--- src/magic.rs
+++ src/magic.rs
@@ -1,39 +1,62 @@
 use crate::bitboard::Bitboard;
 use std::vec::Vec;
 
-pub struct MagicTable {
-    pub rookMagic: [Magic; 64],
-    pub bishopMagic: [Magic; 64]
+pub struct MagicTable<'a> {
+    pub rookMagic: [Magic<'a>; 64],
+    pub bishopMagic: [Magic<'a>; 64]
 }
 
 //All the information needed to compute magic attacks coming from one square.
-pub struct Magic {
+#[derive(Clone, Copy)]
+pub struct Magic<'a> {
     //A mask which, when &ed with the occupancy bitboard, will give only the
     //bits that matter when computing moves.
     pub mask: Bitboard,
     //The magic number to multiply to hash the current board effectively
     pub magic: Bitboard,
-    pub attacks: Vec<Bitboard>,
+    pub attacks: &'a Vec<Bitboard>,
     pub shift: u8
 }
 
-pub fn makeRookMagic() -> [Magic; 64] {
-    let m_placeholder = Magic{
-        mask: Bitboard(0), 
-        magic: Bitboard(0), 
-        attacks: Vec::new(), 
-        shift: 0,
-    };
-    let mut outArray = [m_placeholder; 64];
-    let mut obsBbValue = 1u64;
+pub fn makeMagic<'a>(table: MagicTable) {
+
+}
+
+pub fn makeRookMagic<'a>(rookTable :&'a mut [Magic; 64]) {
     for i in 0..64 {
-        //make the board corresponding to this
-
+        //sequence of 1s down the same row as the piece to move
+        let row_mask = Bitboard(0xFF << (8 * (i / 8)));
+        //sequence of 1s down the same col as the piece to move
+        let col_mask = Bitboard(0x0101010101010101 << (i % 8));
+        //in the col mask or row mask, but not the piece to move
+        let mask = (row_mask | col_mask) & Bitboard(!(1 << i));
+        rookTable[i].mask = mask;
     }
-    return outArray;
 }
 
-pub const fn makeBishopMagic() -> [Magic; 64] {
-
+pub fn makeBishopMagic<'a>(bishopTable: &'a mut [Magic; 64]) {
+    makeRookMagic(bishopTable)
 }
 
+
+#[cfg(test)]
+mod tests {
+    #[allow(dead_code)]
+    use super::*;
+
+    #[test]
+    fn testRookMagicMask() {
+        let m_placeholder = Magic{
+            mask: Bitboard(0), 
+            magic: Bitboard(0), 
+            attacks: &Vec::new(), 
+            shift: 0,
+        };
+        let mut outArray = [m_placeholder; 64];
+        makeRookMagic(&mut outArray);
+        //println!("{:064b}", outArray[0].mask.0);
+        assert_eq!(outArray[0].mask, Bitboard(0x01010101010101FE));
+        assert_eq!(outArray[4].mask, Bitboard(0x10101010101010EF));
+        assert_eq!(outArray[36].mask, Bitboard(0x101010EF10101010));
+    }
+}
