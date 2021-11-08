25cef9cde9829e1ba8662994b1a528c3124aafe5
claytonwramsey <claytonwramsey@gmail.com>
2021-11-08 13:25:46 -0600

    rethought engine behavior

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -9,7 +9,10 @@ use std::collections::HashMap;
 pub mod eval;
 
 /**
- * The public functions that we can trust every Engine to have.
+ * An `Engine` is something that can evaluate a `Game`, and give moves which it 
+ * thinks are good. All the public methods require it to be mutable so that the 
+ * engine can alter its internal state (such as with transposition tables) to 
+ * update its internal data.
  */
 pub trait Engine {
     /**
@@ -20,15 +23,15 @@ pub trait Engine {
     /**
      * Evaluate the position of the given game.
      */
-    fn evaluate(g: &Game, mgen: &MoveGenerator) -> Eval;
+    fn evaluate(&mut self, g: &Game, mgen: &MoveGenerator) -> Eval;
 
     /**
      * Get what this engine believes to be the best move in the given position.
      */
-    fn get_best_move(g: &Game, mgen: &MoveGenerator) -> Move;
+    fn get_best_move(&mut self, g: &Game, mgen: &MoveGenerator) -> Move;
 
     /**
      * Get the evaluation of each move in this position.
      */
-    fn get_evals(g: &Game, mgen: &MoveGenerator) -> HashMap<Move, Eval>;
+    fn get_evals(&mut self, g: &Game, mgen: &MoveGenerator) -> HashMap<Move, Eval>;
 }
