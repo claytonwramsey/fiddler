0d1125a9099d5ef30cf923edbc1cd7777e953e0d
Clayton Ramsey <claytonwramsey@gmail.com>
2021-12-20 16:19:00 -0800

    playing with CLI

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ use crabchess::Game;
 use crabchess::MoveGenerator;
 
 
-//use crabchess::cli;
+use crabchess::cli;
 fn main() {
-    let mut g = Game::from_fen("rnbqkbnr/ppp1pp1p/6p1/3pP3/3P4/8/PPP2PPP/RNBQKBNR b KQkq - 0 3").unwrap();
+    /*let mut g = Game::from_fen("rnbqkbnr/ppp1pp1p/6p1/3pP3/3P4/8/PPP2PPP/RNBQKBNR b KQkq - 0 3").unwrap();
     let mgen = MoveGenerator::new();
     let mut e = Minimax::default();
 
     let x = e.get_evals(&mut g, &mgen);
-    println!("{:?}", x);
+    println!("{:?}", x);*/
 
-    /*println!("running!");
+    println!("running!");
     let mut app = cli::CrabchessApp::default();
     if let Err(_) = app.run() {
         println!("app failed!");
-    }*/
+    }
 }
