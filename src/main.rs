bb471d0984777ee573ed2245ac5d80905abc879a
Clayton Ramsey <claytonwramsey@gmail.com>
2022-03-18 22:00:38 -0500

    fried liver perft mode updated

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 #[allow(unused_imports)]
 use crabchess::base::Game;
 #[allow(unused_imports)]
-use crabchess::engine::search::PVSearch;
-
+use crabchess::engine::thread::MainSearch;
 #[allow(unused_imports)]
 use crabchess::cli;
+
 fn main() {
     /*crabchess::base::perft::perft(
         "r1bq1b1r/ppp2kpp/2n5/3np3/2B5/8/PPPP1PPP/RNBQK2R w KQ - 0 7",
         5,
     );*/
-    /*let mut g =
-        Game::from_fen("r1bq1b1r/ppp2kpp/2n5/3np3/2B5/8/PPPP1PPP/RNBQK2R w KQ - 0 7").unwrap();
+    let g = Game::from_fen("r1bq1b1r/ppp2kpp/2n5/3np3/2B5/8/PPPP1PPP/RNBQK2R w KQ - 0 7").unwrap();
 
-    let mut e = PVSearch::default();
+    let mut e = MainSearch::new();
     e.set_depth(10);
+    e.set_nhelpers(15);
 
-    let m = e.best_move(&mut g, &mgen, &crabchess::engine::NoTimeout);
-    println!("{:?}", m);*/
+    let r = e.evaluate(&g);
+    println!("{:?}", r);
 
-    println!("running!");
+    /*println!("running!");
     let mut app = cli::CrabchessApp::default();
     if app.run().is_err() {
         println!("app failed!");
-    }
+    }*/
 }
