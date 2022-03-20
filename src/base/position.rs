b202008addc780885025af18ed5185ef4b8ada74
Clayton Ramsey <claytonwramsey@gmail.com>
2022-03-20 16:20:24 -0500

    created PST deltas

--- src/base/position.rs
+++ src/base/position.rs
@@ -8,21 +8,26 @@ pub struct Position {
 }
 
 impl Position {
-    /// 
+    /// Construct a position from a FEN. The resulting PST value will be 
+    /// uninitialized, so it must be later updated accordingly.
     pub fn from_fen(fen: &str) -> Result<Position, &str> {
         let board = Board::from_fen(fen)?;
         Ok(Position {
             board,
-            check_info: CheckInfo::about(&b),
-            pst_val: todo!("refactor to use PST"),
+            check_info: CheckInfo::about(&board),
+            pst_val: (Eval::DRAW, Eval::DRAW),
         })
     }
 
     #[inline]
-    /// Make a move on this position, updating 
-    pub fn make_move(&mut self, m: Move) {
+    /// Make a move on this position, updating the check info and PST values as 
+    /// needed.
+    pub fn make_move(&mut self, m: Move, pst_delta: (Eval, Eval)) {
         self.board.make_move(m);
         self.check_info = CheckInfo::about(&self.board);
-        todo!("refactor to use PST");
+        self.pst_val = (
+            self.pst_val.0 + pst_delta.0,
+            self.pst_val.1 + pst_delta.1
+        );
     }
 }
