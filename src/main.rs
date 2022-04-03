c282ffbf8e69059ec025fe0e6e22be07779d3638
Clayton Ramsey <claytonwramsey@gmail.com>
2022-04-02 23:07:39 -0500

    branching main

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,53 @@
-#[allow(unused_imports)]
+use std::env;
+
 use crabchess::base::Game;
-#[allow(unused_imports)]
-use crabchess::cli;
-#[allow(unused_imports)]
+use crabchess::base::perft::perft;
+use crabchess::cli::CrabchessApp;
 use crabchess::engine::pst::pst_evaluate;
-#[allow(unused_imports)]
 use crabchess::engine::thread::MainSearch;
 
 fn main() {
-    /*crabchess::base::perft::perft(
-        "r1bq1b1r/ppp2kpp/2n5/3np3/2B5/8/PPPP1PPP/RNBQK2R w KQ - 0 7",
-        5,
-    );*/
-    /*let g = Game::from_fen(
-        "2r2r2/3p1p1k/p3p1p1/3P3n/q3P1Q1/1p5P/1PP2R2/1K4R1 w - - 0 30",
-        pst_evaluate,
-    )
-    .unwrap();
-
-    let mut e = MainSearch::new();
-    e.set_depth(6);
-    e.set_nhelpers(15);
-
-    let r = e.evaluate(&g);
-    println!("{:?}", r);*/
-
-    println!("running!");
-    let mut app = cli::CrabchessApp::default();
-    if app.run().is_err() {
-        println!("app failed!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        match args[1].as_str() {
+            "perft" => {
+                if args.len() >= 3 {
+                    // args[1] is the depth, args[2..] is the FEN
+                    let depth = args[2].parse::<u8>().unwrap();
+                    let fen = args[3..].join(" ");
+                    perft(&fen, depth);
+                } else {
+                    println!("please specify a depth and a FEN");
+                }
+            },
+            "cli" => {
+                // Run the CLI application.
+                let mut app = CrabchessApp::default();
+                if app.run().is_err() {
+                    println!("app failed!");
+                }
+            }
+            "bench" => {
+                // for now, just run a benchmark on the fried liver fen
+                let g = Game::from_fen(
+                    "2r2r2/3p1p1k/p3p1p1/3P3n/q3P1Q1/1p5P/1PP2R2/1K4R1 w - - 0 30",
+                    pst_evaluate,
+                )
+                .unwrap();
+            
+                let mut e = MainSearch::new();
+                e.set_depth(9);
+                e.set_nhelpers(15);
+            
+                let r = e.evaluate(&g);
+                println!("{:?}", r);
+            }
+            _ => {
+                println!("unrecognized mode of operation {:?}", args[0]);
+            }
+        };
+    } else {
+        // no arguments; for now just exit
+        println!("modes: cli, perft, bench");
     }
 }
