fc88eb73d5a34b1eeeb0497f488c6c07c53d2419
Clayton Ramsey <claytonwramsey@gmail.com>
2023-06-06 18:41:57 -0500

    try disabling prefetch

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -389,7 +389,7 @@ impl<'a> PVSearch<'a> {
             self.game.make_move(tm.m);
             // Prefetch the next transposition table entry as early as possible
             // (~12 Elo)
-            self.ttable.prefetch(self.game.meta().hash);
+            // self.ttable.prefetch(self.game.meta().hash);
 
             let mut score = Eval::MIN;
 
@@ -604,7 +604,7 @@ impl<'a> PVSearch<'a> {
             self.game.make_move(tm.m);
             // Prefetch the next transposition table entry as early as possible
             // (~12 Elo)
-            self.ttable.prefetch(self.game.meta().hash);
+            // self.ttable.prefetch(self.game.meta().hash);
             // zero-window search
             score = -self.quiesce::<false>(-alpha - Eval::centipawns(1), -alpha, &mut new_state)?;
             if PV && alpha < score && score < beta {
