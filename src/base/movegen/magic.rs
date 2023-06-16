64b7651a2c2bd2951b04c343ec58adffa9f9623f
Clayton Ramsey <claytonwramsey@gmail.com>
2023-06-16 17:09:58 -0500

    add attribution comments for magic number sources

--- src/base/movegen/magic.rs
+++ src/base/movegen/magic.rs
@@ -140,22 +140,22 @@ const SAVED_ROOK_MAGICS: [u64; 64] = [
     0x2001_0200_0400_8080, // f6
     0x1000_0801_100c_001a, // g6
     0x0048_0082_5402_0011, // h6
-    0x48FF_FE99_FECF_AA00, // a7
-    0x48FF_FE99_FECF_AA00, // b7
-    0x497F_FFAD_FF9C_2E00, // c7
-    0x613F_FFDD_FFCE_9200, // d7
-    0xffff_ffe9_ffe7_ce00, // e7
-    0xffff_fff5_fff3_e600, // f7
-    0x0003_ff95_e5e6_a4c0, // g7
-    0x510F_FFF5_F63C_96A0, // h7
-    0xEBFF_FFB9_FF9F_C526, // a8
-    0x61FF_FEDD_FEED_AEAE, // b8
-    0x53BF_FFED_FFDE_B1A2, // c8
-    0x127F_FFB9_FFDF_B5F6, // d8
-    0x411F_FFDD_FFDB_F4D6, // e8
+    0x48FF_FE99_FECF_AA00, // a7, found by Grant Osborne
+    0x48FF_FE99_FECF_AA00, // b7, found by Grant Osborne
+    0x497F_FFAD_FF9C_2E00, // c7, found by Grant Osborne
+    0x613F_FFDD_FFCE_9200, // d7, found by Grant Osborne
+    0xffff_ffe9_ffe7_ce00, // e7, found by Volker Annuss
+    0xffff_fff5_fff3_e600, // f7, found by Volker Annuss
+    0x0003_ff95_e5e6_a4c0, // g7, found by Niklas Fiekas
+    0x510F_FFF5_F63C_96A0, // h7, found by Grant Osborne
+    0xEBFF_FFB9_FF9F_C526, // a8, found by Grant Osborne
+    0x61FF_FEDD_FEED_AEAE, // b8, found by Grant Osborne
+    0x53BF_FFED_FFDE_B1A2, // c8, found by Grant Osborne
+    0x127F_FFB9_FFDF_B5F6, // d8, found by Grant Osborne
+    0x411F_FFDD_FFDB_F4D6, // e8, found by Grant Osborne
     0x0822_0024_0810_4502, // f8
-    0x0003_ffef_27ee_be74, // g8
-    0x7645_FFFE_CBFE_A79E, // h8
+    0x0003_ffef_27ee_be74, // g8, found by Peter Österlund 
+    0x7645_FFFE_CBFE_A79E, // h8, found by Grant Osborne
 ];
 
 /// A saved list of magics for bishops created using the generator.
@@ -163,30 +163,30 @@ const SAVED_ROOK_MAGICS: [u64; 64] = [
 /// Some magics for sizes below the required bitshift amount were taken from the Chess Programming
 /// Wiki.
 const SAVED_BISHOP_MAGICS: [u64; 64] = [
-    0xffed_f9fd_7cfc_ffff, // a1
-    0xfc09_6285_4a77_f576, // b1
+    0xffed_f9fd_7cfc_ffff, // a1, found by Gerd Isenberg
+    0xfc09_6285_4a77_f576, // b1, found by Gerd Isenberg
     0x0012_2808_c102_a004, // c1
     0x2851_2400_8240_0440, // d1
     0x0011_1040_1100_0202, // e1
     0x0008_2208_2000_0010, // f1
-    0xfc0a_66c6_4a7e_f576, // g1
-    0x7ffd_fdfc_bd79_ffff, // h1
-    0xfc08_46a6_4a34_fff6, // a2
-    0xfc08_7a87_4a3c_f7f6, // b2
+    0xfc0a_66c6_4a7e_f576, // g1, found by Gerd Isenberg
+    0x7ffd_fdfc_bd79_ffff, // h1, found by Gerd Isenberg
+    0xfc08_46a6_4a34_fff6, // a2, found by Gerd Isenberg
+    0xfc08_7a87_4a3c_f7f6, // b2, found by Gerd Isenberg
     0x0009_8802_0420_a000, // c2
     0x8000_4404_0080_8200, // d2
     0x208c_8450_c001_3407, // e2
     0x1980_1105_2010_8030, // f2
-    0xfc08_64ae_59b4_ff76, // g2
-    0x3c08_60af_4b35_ff76, // h2
-    0x73C0_1AF5_6CF4_CFFB, // a3
-    0x41A0_1CFA_D64A_AFFC, // b3
+    0xfc08_64ae_59b4_ff76, // g2, found by Gerd Isenberg
+    0x3c08_60af_4b35_ff76, // h2, found by Gerd Isenberg
+    0x73C0_1AF5_6CF4_CFFB, // a3, found by Richard Pijl
+    0x41A0_1CFA_D64A_AFFC, // b3, found by Richard Pijl
     0x0604_0002_04a2_0202, // c3
     0x0002_8208_0602_4000, // d3
     0x008a_0024_2201_0201, // e3
     0x2082_0040_8801_0802, // f3
-    0x7c0c_028f_5b34_ff76, // g3
-    0xfc0a_028e_5ab4_df76, // h3
+    0x7c0c_028f_5b34_ff76, // g3, found by Gerd Isenberg
+    0xfc0a_028e_5ab4_df76, // h3, found by Gerd Isenberg
     0x0810_0420_d104_1080, // a4
     0x0904_5100_0210_0100, // b4
     0x0202_2808_0406_4403, // c4
@@ -203,30 +203,30 @@ const SAVED_BISHOP_MAGICS: [u64; 64] = [
     0x2190_4102_0000_4058, // f5
     0x0188_8214_0180_8080, // g5
     0x2006_0a02_0000_c4c0, // h5
-    0xDCEF_D9B5_4BFC_C09F, // a6
-    0xF95F_FA76_5AFD_602B, // b6
+    0xDCEF_D9B5_4BFC_C09F, // a6, found by Richard Pijl
+    0xF95F_FA76_5AFD_602B, // b6, found by Richard Pijl
     0x200a_1041_1000_2040, // c6
     0x0800_000c_0831_0c00, // d6
     0x0218_0401_0a01_0400, // e6
     0x1092_2004_0022_4100, // f6
-    0x43ff_9a5c_f4ca_0c01, // g6
-    0x4BFF_CD8E_7C58_7601, // h6
-    0xfc0f_f286_5334_f576, // a7
-    0xfc0b_f6ce_5924_f576, // b7
+    0x43ff_9a5c_f4ca_0c01, // g6, found by Gerd Isenberg
+    0x4BFF_CD8E_7C58_7601, // h6, found by Richard Pijl
+    0xfc0f_f286_5334_f576, // a7, found by Gerd Isenberg
+    0xfc0b_f6ce_5924_f576, // b7, found by Gerd Isenberg
     0x8052_2060_8c30_0001, // c7
     0x2084_1050_4202_0400, // d7
     0xe018_8010_2206_0220, // e7
     0x0001_1220_4901_0200, // f7
-    0xc3ff_b7dc_36ca_8c89, // g7
-    0xc3ff_8a54_f4ca_2c89, // h7
-    0xffff_fcfc_fd79_edff, // a8
-    0xfc08_63fc_cb14_7576, // b8
+    0xc3ff_b7dc_36ca_8c89, // g7, found by Gerd Isenberg
+    0xc3ff_8a54_f4ca_2c89, // h7, found by Gerd Isenberg
+    0xffff_fcfc_fd79_edff, // a8, found by Gerd Isenberg
+    0xfc08_63fc_cb14_7576, // b8, found by Gerd Isenberg
     0x40a0_0400_6213_3000, // c8
     0x0142_0280_0084_0400, // d8
     0x0009_0900_1006_1200, // e8
     0x0800_8445_2810_0308, // f8
-    0xfc08_7e8e_4bb2_f736, // g8
-    0x43ff_9e4e_f4ca_2c89, // h8
+    0xfc08_7e8e_4bb2_f736, // g8, found by Gerd Isenberg
+    0x43ff_9e4e_f4ca_2c89, // h8, found by Gerd Isenberg
 ];
 
 /// The number of bits used to express the magic lookups for rooks at each square.
